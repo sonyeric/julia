c8b49d678c3e10ce2ce869a8b956fe8d8a18fcf4
Elliot Saba <staticfloat@gmail.com>
2020-05-02 13:05:45 -0700

    Address review comments

--- contrib/windows/exe_path_wrapper.c
+++ contrib/windows/exe_path_wrapper.c
@@ -2,7 +2,9 @@
 #include <tchar.h>
 #include <stdio.h>
 #include <shlwapi.h>
-#define ENVVAR_MAXLEN 32760
+
+/* The maximum path length we'll allow */
+#define MAX_PATH_LEN 32768
 
 /* PATH_ENTRIES is our simulated RPATH, usually of the form 'TEXT("../path1"), TEXT("../path2"), ...' */
 #ifndef PATH_ENTRIES
@@ -20,27 +22,32 @@ LPWSTR pathEntries[] = {
 
 int wmain(int argc, wchar_t *argv[], wchar_t *envp[]) {
     // Determine absolute path to true julia.exe sitting in `libexec/`
-    WCHAR currFileDir[MAX_PATH];
-    WCHAR juliaPath[MAX_PATH];
-    GetModuleFileName(NULL, currFileDir, MAX_PATH);
+    WCHAR currFileDir[MAX_PATH_LEN];
+    WCHAR juliaPath[MAX_PATH_LEN];
+    if (!GetModuleFileName(NULL, currFileDir, MAX_PATH_LEN)) {
+        fprintf(stderr, "ERROR: GetModuleFileName() failed with code %d\n", GetLastError());
+        exit(1);
+    }
     PathRemoveFileSpec(currFileDir);
     PathCombine(juliaPath, currFileDir, TEXT(JULIA_EXE_PATH));
 
     // On windows, we simulate RPATH by pushing onto PATH
-    LPWSTR pathVal = (LPWSTR) malloc(ENVVAR_MAXLEN*sizeof(WCHAR));
-    DWORD dwRet = GetEnvironmentVariable(TEXT("PATH"), pathVal, ENVVAR_MAXLEN);
+    LPWSTR pathVal = (LPWSTR) malloc(MAX_PATH_LEN*sizeof(WCHAR));
+    DWORD dwRet = GetEnvironmentVariable(TEXT("PATH"), pathVal, MAX_PATH_LEN);
     DWORD numPathEntries = sizeof(pathEntries)/sizeof(pathEntries[0]);
     if (dwRet == 0) {
         // If we cannot get PATH, then our job is easy!
         pathVal[0] = '\0';
-    } else {
+    }
+    else {
         // Otherwise, we append, if we have enough space to:
         DWORD currFileDirLen = wcslen(currFileDir);
         DWORD totalPathLen = dwRet + 1 + currFileDirLen;
-        for (int env_idx=0; env_idx<numPathEntries; ++env_idx) {
+        int env_idx;
+        for (env_idx = 0; env_idx < numPathEntries; ++env_idx) {
             totalPathLen += 1 + currFileDirLen + 1 + wcslen(pathEntries[env_idx]);
         }
-        if (ENVVAR_MAXLEN < totalPathLen ) {
+        if (MAX_PATH_LEN < totalPathLen) {
             fprintf(stderr, "ERROR: Cannot append entries to PATH: not enough space in environment block.  Reduce size of PATH!");
             exit(1);
         }
@@ -50,7 +57,8 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[]) {
     lstrcat(pathVal, currFileDir);
 
     // For each entry in PATH_ENTRIES, tack it on to the end, relative to the current directory:
-    for (int env_idx=0; env_idx<numPathEntries; ++env_idx) {
+    int env_idx;
+    for (env_idx = 0; env_idxi < numPathEntries; ++env_idx) {
         lstrcat(pathVal, TEXT(";"));
         lstrcat(pathVal, currFileDir);
         lstrcat(pathVal, TEXT("\\"));
